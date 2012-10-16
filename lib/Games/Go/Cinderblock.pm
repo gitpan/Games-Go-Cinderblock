0bebcddad359dd3ff819b26482943bc8e37b26eb
Zach Morgan <zpmorgan@cpan.org>
2012-10-16 09:59:34 +0000

    Import of ZPMORGAN/Games-Go-Cinderblock-0.05 from CPAN.
    gitpan-cpan-distribution: Games-Go-Cinderblock
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         ZPMORGAN/Games-Go-Cinderblock-0.05.tar.gz
    gitpan-cpan-author:       ZPMORGAN
    gitpan-cpan-maturity:     released

---
+++ lib/Games/Go/Cinderblock.pm
@@ -0,0 +1,8 @@
+package Games::Go::Cinderblock;
+
+# ABSTRACT: Implementing a myriad of Go rulesets
+
+
+our $VERSION = 0.05;
+
+1;
---
+++ lib/Games/Go/Cinderblock/MoveResult.pm
@@ -0,0 +1,97 @@
+package Games::Go::Cinderblock::MoveResult;
+use Moose;
+
+has move_attempt => (
+   isa => 'Games::Go::Cinderblock::MoveAttempt', # Games::Go::Cinderblock::MoveAttempt?
+   is => 'ro',
+   required => 1,
+);
+
+has rulemap => (
+   isa => 'Games::Go::Cinderblock::Rulemap',
+   is => 'ro', #shouldn't change.
+   required => 1,
+);
+has basis_state=> (
+   is => 'ro',
+   required => 1,
+   isa => 'Games::Go::Cinderblock::State',
+);
+
+has resulting_state=> (
+   is => 'ro',
+   # required if success?
+   isa => 'Games::Go::Cinderblock::State',
+);
+has delta => (
+   lazy => 1,
+   is => 'ro',
+   builder => '_derive_delta',
+   isa => 'HashRef', # Games::Go::Cinderblock::Delta
+);
+
+has succeeded => (
+   isa => 'Bool',
+   is => 'ro',
+#   lazy => 1,
+#   builder => '_determine_success',
+   required => 1,
+);
+has reason => (
+   isa => 'Str',
+   is => 'ro',
+   required => 0,
+);
+
+sub _derive_delta{
+   my $self = shift;
+   return $self->rulemap->delta(
+      $self->basis_state->board,
+      $self->resulting_state->board,
+   );
+}
+
+# meh
+sub failed{
+   my $self = shift;
+   return ($self->succeeded ? 0 : 1);
+}
+1;
+
+__END__
+
+=head1 NAME
+
+Games::Go::Cinderblock::MoveResult
+
+=head1 DESCRIPTION
+
+This results from evaluate_move.
+
+It has a L</delta>, which describes changes to the board
+
+=head1 ATTRIBUTES
+
+=head2 rulemap 
+
+=head2 succeeded
+
+0 if move failed, 1 if move succeeded.
+
+=head2 reason
+
+If failed, this is why. e.g. 'suicide', 'collosion'
+
+=head2 basis_state
+
+=head2 resulting_state
+
+=head2 delta
+
+L</resulting_state> minus L</basis_state>, essentially.
+
+A delta is a hashref with 'remove' & 'add' keys;
+its values are lists of [color,node] pairs...
+
+=cut
+
